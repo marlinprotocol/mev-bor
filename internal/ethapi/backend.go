fdba630ae152a86f2b2c75df0513e639e5788400
whitewalker608 <nitingoel608@gmail.com>
2021-07-24 11:20:31 +0530

    lin rpc api

--- internal/ethapi/backend.go
+++ internal/ethapi/backend.go
@@ -158,6 +158,11 @@ func GetAPIs(apiBackend Backend, chain *core.BlockChain) []rpc.API {
 			Version:   "1.0",
 			Service:   NewBundleAPI(apiBackend, chain),
 			Public:    true,
+		}, {
+			Namespace: "lin",
+			Version:   "1.0",
+			Service:   NewMarlinAPI(apiBackend),
+			Public:    true,
 		},
 	}
 }
