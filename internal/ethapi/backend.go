44fa83ff3fa6342cab73e819f91655a3e1d8f6eb
Roshan Raghupathy <thisisroshansmail@gmail.com>
2022-12-22 10:38:02 +0530

    mev_sendBundle support

--- internal/ethapi/backend.go
+++ internal/ethapi/backend.go
@@ -112,6 +112,9 @@ type Backend interface {
 	PurgeWhitelistedCheckpoint()
 	GetWhitelistedMilestone() (bool, uint64, common.Hash)
 	PurgeWhitelistedMilestone()
+
+	// MEV related APIs
+	SendBundle(ctx context.Context, txs types.Transactions, blockNumber rpc.BlockNumber, minTimestamp uint64, maxTimestamp uint64, revertingTxHashes []common.Hash) error
 }
 
 func GetAPIs(apiBackend Backend) []rpc.API {
@@ -139,6 +142,9 @@ func GetAPIs(apiBackend Backend) []rpc.API {
 		}, {
 			Namespace: "personal",
 			Service:   NewPersonalAccountAPI(apiBackend, nonceLock),
+		}, {
+			Namespace: "mev",
+			Service:   NewPrivateTxBundleAPI(apiBackend),
 		},
 	}
 }
