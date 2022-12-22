f7f0581518f77f5cf2864ea25ec4efd8cc5b49e3
Roshan Raghupathy <thisisroshansmail@gmail.com>
2022-12-22 11:14:21 +0530

    reduce recommit interval

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -93,7 +93,7 @@ var Defaults = Config{
 	Miner: miner.Config{
 		GasCeil:  8000000,
 		GasPrice: big.NewInt(params.GWei),
-		Recommit: 125 * time.Second,
+		Recommit: 700 * time.Millisecond,
 	},
 	TxPool:             core.DefaultTxPoolConfig,
 	RPCGasCap:          50000000,
